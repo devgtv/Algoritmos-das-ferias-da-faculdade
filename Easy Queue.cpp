c89f78e64935351d746c30b7260c3b63128cd332
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-07-15 17:16:39 -0300

    fila facil

---
+++ Easy Queue.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    queue<long long> fila;
+
+    while (t--) {
+        int op;
+        cin >> op;
+
+        if (op == 1) {
+            long long n;
+            cin >> n;
+            fila.push(n);
+        }
+        else if (op == 2) {
+            if (!fila.empty()) {
+                fila.pop();
+            }
+        }
+        else if (op == 3) {
+            if (fila.empty()) {
+                cout << "Empty!" << '\n';
+            } else {
+                cout << fila.front() << '\n';
+            }
+        }
+    }
+
+    return 0;
+}
