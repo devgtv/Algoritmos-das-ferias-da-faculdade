b342e2d60fb24678b3bba81c6a33bdcc4a548699
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-07-15 17:14:36 -0300

    stack facil

---
+++ Easy Stack.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    stack<long long> pilha;
+    while (t--) {
+        int fila;
+        cin >> fila;
+        if (fila == 1) {
+            long long n;
+            cin >> n;
+            pilha.push(n);
+        }
+        else if (fila == 2) {
+            if (!pilha.empty()) {
+                pilha.pop();
+            }
+        }
+        else if (fila == 3) {
+            if (pilha.empty()) {
+                cout << "Empty!" << "\n";
+            } else {
+                cout << pilha.top() << "\n";
+            }
+        }
+    }
+    return 0;
+}
